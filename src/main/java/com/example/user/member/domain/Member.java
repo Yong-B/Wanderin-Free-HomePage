74308a3ff872094eabd6dad4530d86f63f9dd0f3
choiyongbum <72883647+choiyongbum@users.noreply.github.com>
2025-10-11 00:19:04 +0900

    Feat : 회원 테이블 및 회원 엔티티

---
+++ src/main/java/com/example/user/member/domain/Member.java
@@ -0,0 +1,36 @@
+﻿package com.example.user.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Member {
+
+    @Id
+    private Long id;
+
+    @Column(name = "login_Id")
+    private String loginId;
+    
+    private String password;
+    
+    private String name;
+    
+    private String email;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/resources/db/migration/V1_1__create_users_table.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE IF NOT EXISTS member (
+    id         BIGSERIAL PRIMARY KEY,
+    login_id   VARCHAR(50) UNIQUE NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    name       VARCHAR(100) NOT NULL,
+    email      VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 테이블 코멘트
+COMMENT ON TABLE member IS '회원 정보';
+
+-- 컬럼 코멘트
+COMMENT ON COLUMN member.login_id IS '회원 ID (로그인용)';
+COMMENT ON COLUMN member.password IS '비밀번호 (해시 암호화)';
+COMMENT ON COLUMN member.name IS '회원 이름';
+COMMENT ON COLUMN member.email IS '이메일';
+COMMENT ON COLUMN member.created_at IS '회원 생성 시각';
+COMMENT ON COLUMN member.updated_at IS '회원 정보 수정 시각';
